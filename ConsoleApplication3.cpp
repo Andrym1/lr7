cd851eaf9a7902dbd7b712320e4e70ab7bedca15
Your Name <you@example.com>
2024-12-03 14:00:47 +0200

    first commit

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath> 
+#include "windows.h"
+
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double x = 2.0;
+
+    cout << "Результати обчислень:\n";
+
+    do
+    {
+        double y = pow(cos(x), 2) + log(x) + 2; 
+        cout << "X = " << x << "\t";
+        cout << "Y = " << y << endl;
+
+        x += 0.1; 
+    } while (x <= 3); 
+
+    system("pause");
+    return 0;
+}
